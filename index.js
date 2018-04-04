8e77ffac1b115043a4501650676359eba8b55b72
Johnathan <johnathan.hair.au@gmail.com>
2018-04-04 13:19:42 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var AWSXRay = require('aws-xray-sdk');
 
-module.exports = AWSXRay.capturePostgres(require('pg'));
+if (process.env.AWS_PATH && process.env.AWS_XRAY_TRACING_NAME) {
+  AWSXRay.config([AWSXRay.plugins.EC2Plugin, AWSXRay.plugins.ElasticBeanstalkPlugin]);
+  module.exports = AWSXRay.capturePostgres(require('pg'));
+} else {
+  module.exports = require('pg');
+}
